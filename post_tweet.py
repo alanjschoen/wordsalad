e1f2d7f6e27c0b3a127a410301aa7c64c064351c
Alan Schoen <alan.schoen@gmail.com>
2016-10-09 16:12:47 -0400

    Remove old comments and imports

--- post_tweet.py
+++ post_tweet.py
@@ -1,27 +1,15 @@
-import os
+import cnfg
 import tweepy
-import numpy as np
-
-# sentences = []
-# while len(sentences) < 2:
-#     textfile = os.popen('python markov.py text_samples/obama.txt 200')
-#     sentences = textfile.read().strip().split('.')
-#     sentences = [s.strip() for s in sentences]
-#     sentences =  [s + '.' for s in sentences[1:] if len(s) < 140]
-# tweet_text = sentences[np.argmax(map(len, sentences))]
-
-
 from sk_markov import generate_tweet
+
 tweet_text = generate_tweet()
 print tweet_text
 
-import cnfg
 config = cnfg.load(".twitter_config_robama")
 auth = tweepy.OAuthHandler(config["consumer_key"],
                            config["consumer_secret"])
 auth.set_access_token(config["access_token"],
                       config["access_token_secret"])
 api=tweepy.API(auth)
-
 
 api.update_status(tweet_text)
